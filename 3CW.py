90a13a6571b6d567c174d12dece2684d9d7e93b8
Дмитро Кот <Kot@rivne.itstep.org>
2020-01-21 21:07:56 +0200

    first commit

---
+++ 3CW.py
@@ -0,0 +1,116 @@
+# from mod.calc import plus, minus, multiple, division
+
+# # res = plus(2, 2)
+# # print("Result =>", res)
+
+# # res = minus(2, 2)
+# # print("Result =>", res)
+
+# print("Calculate a ? b")
+# def Calculate():
+#     a = int(input("Enter a : "))
+#     dia = input("Enter + | - | * | / |: ")
+#     b = int(input("Enter b : "))
+
+#     if dia == "+" :
+#         res = plus(a, b)
+#         print(a,"+", b, "=", res)
+
+#     elif dia == "-" :
+#         res = minus(a, b)
+#         print(a,"-", b, "=", res)
+
+#     elif dia == "*" :
+#         res = multiple(a, b)
+#         print(a,"*", b, "=", res)
+
+#     elif dia == "/" :
+#         res = division(a, b)
+#         print(a,"/", b, "=", res)
+
+#     else:
+#         print("Enter correct !!!")
+
+# Calculate()
+
+
+
+
+
+from mod.calc import plus
+
+print("Ready question? GO!")
+def Questions():
+    q1 = int(input("Question_1 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q1 == 1 :
+        print("true")
+        res =+ 1 
+    else:
+        print("false")
+
+    q2 = int(input("Question_2 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q2 == 2 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+    
+    q3 = int(input("Question_3 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble)) ? : "))
+    if q3 == 1 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+
+    q4 = int(input("Question_4 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q4 == 4 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+
+    q5 = int(input("Question_5 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q5 == 3 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+    
+    q6 = int(input("Question_6 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q6 == 3 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+
+    q7 = int(input("Question_7 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q7 == 1 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+    
+    q8 = int(input("Question_8 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q8 == 2 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+
+    q9 = int(input("Question_9 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q9 == 4 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+
+    q10 = int(input("Question_10 : bla bla bla - 1(blo blo), 2(blo blo), 3(blo blo) or 2(ble ble) ? : "))
+    if q10 == 2 :
+        print("true")
+        res =+ 1
+    else:
+        print("false")
+    
+    print("resultat : ", res)
+
+Questions()
---
+++ mod/calc.py
@@ -0,0 +1,32 @@
+# if __name__ == "__main__":
+#     plus()
+#     minus()
+#     multiple()
+#     division()
+
+# def plus(*params):
+#     res = 0
+#     for item in params:
+#         res =+ item
+#     return res
+
+# def minus(a, b):
+#     return a - b
+
+# def multiple(a, b):
+#     return a * b
+
+# def division(a, b):
+#     if b == 0:
+#         return "Error. Division on zero"
+#     else:
+#         return a / b
+
+if __name__ == "__main__":
+    plus()
+
+def plus(*params):
+    res = 0
+    for item in params:
+        res =+ item
+    return res
